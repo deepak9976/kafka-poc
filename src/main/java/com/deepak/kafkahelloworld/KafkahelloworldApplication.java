921ccb226437cd31ec0f3f1cf5fdc05d77cb3511
Deepak Sharma <deepaks@synergy.com>
2019-10-10 16:48:05 +0100

    first commit kafka message flow

---
+++ src/main/java/com/deepak/kafkahelloworld/KafkahelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.kafkahelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkahelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkahelloworldApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/deepak/kafkahelloworld/com/deepak/kafkahelloworld/service/KafkaWebController.java
@@ -0,0 +1,23 @@
+package com.deepak.kafkahelloworld.com.deepak.kafkahelloworld.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/javainuse-kafka")
+public class KafkaWebController {
+
+    @Autowired
+    KafkaSender kafkaSender;
+
+    @GetMapping(value = "/producer")
+    public String producer(@RequestParam("message") String message){
+        kafkaSender.send(message);
+        return "message sent successfully";
+    }
+
+}
